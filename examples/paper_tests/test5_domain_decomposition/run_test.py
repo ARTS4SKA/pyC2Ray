07ed51694adb2c83e0e4ba4fe8298d254d425d66
ChristopherBignamini <christopher.bignamini@gmail.com>
2026-04-30 11:13:14 +0200

    Example updated with plotting

--- examples/paper_tests/test5_domain_decomposition/run_test.py
+++ examples/paper_tests/test5_domain_decomposition/run_test.py
@@ -2,28 +2,17 @@ from __future__ import annotations
 
 import mpi4py
 mpi4py.rc.finalize = False
-from mpi4py import MPI  # noqa: E402
+from mpi4py import MPI
 import numpy as np
 
 from pyc2ray.domain.morton_grouping import MortonGroupingParams
 from pyc2ray.domain.regular_grid import RegularGrid
-from pyc2ray.domain.sources import Source
+from pyc2ray.domain.sources import Source, SourceGroup
 from pyc2ray.domain.subdomain import Subdomain
+from pyc2ray.domain.utils import get_domain_logger
+from pyc2ray.visualization.domain_decomposition import plot_domain_decomposition
 
-# def generate_sources(num_cluster_sources: int, cluster_center: np.ndarray, cluster_width: float,
-#                      num_sparse_sources: int, source_strength: float, r_max_lls: float, boxsize: float) -> List[Source]:
-#     """Generate a list of sources for the toy example, with a clustered distribution and a sparse distribution.
-#     """
-#     sources = []
-#     # Clustered sources
-#     for i in range(num_cluster_sources):
-#         pos = cluster_center + np.random.uniform(-cluster_width/2, cluster_width/2, size=3)
-#         sources.append(Source(position=pos, luminosity=source_strength))
-#     # Sparse sources
-#     for i in range(num_sparse_sources):
-#         pos = np.random.uniform(0, boxsize, size=3)
-#         sources.append(Source(position=pos, luminosity=source_strength))
-#     return sources
+logger = get_domain_logger(__name__)
 
 def main():
     """Run a complete toy example: source generation, grouping, and plotting."""
@@ -36,20 +25,41 @@ def main():
 
     # Create source list
     sources = [
-        Source(id=0, pos=np.array([0.5, 0.5, 0.5]), strength=1.0, radius=1.0),
-        Source(id=1, pos=np.array([1.5, 1.5, 1.5]), strength=1.0, radius=1.0),
-        Source(id=2, pos=np.array([0.5, 1.5, 0.5]), strength=1.0, radius=1.0),
-        Source(id=3, pos=np.array([0.5, 0.5, 1.5]), strength=1.0, radius=1.0),
-        Source(id=4, pos=np.array([1.0, 1.0, 1.0]), strength=1.0, radius=1.0),
+        Source(id=0, pos=np.array([10.5, 10.5, 10.5]), strength=1.0, radius=1.0),
+        Source(id=1, pos=np.array([11.5, 11.5, 11.5]), strength=1.0, radius=1.0),
+        Source(id=2, pos=np.array([10.5, 11.5, 10.5]), strength=1.0, radius=1.0),
+        Source(id=3, pos=np.array([10.5, 10.5, 11.5]), strength=1.0, radius=1.0),
+        Source(id=4, pos=np.array([10.0, 10.0, 10.0]), strength=1.0, radius=1.0),
         Source(id=5, pos=np.array([60.0, 60.0, 60.0]), strength=1.0, radius=1.0),
         Source(id=6, pos=np.array([70.0, 70.0, 70.0]), strength=1.0, radius=1.0),
     ]
 
+    if subdomain.rank == 0:
+        logger.info("Running domain decompositon with {} ranks.".format(subdomain.comm.Get_size()))
+
     # Run the domain decomposition
     subdomain.run_decomposition(global_grid, sources, grouping_algorithm="morton", 
-                                grouping_params = MortonGroupingParams(max_num_sources_per_group=2, 
-                                                                       max_cost_per_group=2.0, 
-                                                                       morton_bits=5))
+                                grouping_params = MortonGroupingParams(max_num_sources_per_group=5,
+                                                                       max_cost_per_group=1.0,
+                                                                       morton_bits=10))
+
+    # Collect source groups from all ranks
+    source_groups: list[SourceGroup] | None = subdomain.comm.gather(
+        subdomain.get_source_group(), root=0
+    )
+
+    # Collect local grids from all ranks
+    local_grids: list[RegularGrid] | None = subdomain.comm.gather(
+        subdomain.get_local_grid(), root=0
+    )
+
+    # Plot domain decomposition
+    if subdomain.rank == 0:
+        assert source_groups is not None
+        assert local_grids is not None
+        plot_domain_decomposition(global_grid, source_groups, local_grids)
+
+
     MPI.Finalize()
 
 if __name__ == "__main__":
