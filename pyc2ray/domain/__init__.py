5eb6703530dbb574211ca36e2d82854d1ad70849
ChristoperBignamini <christopher.bignamini@gmail.com>
2026-05-11 16:57:39 +0200

    Init updated according to new source files

--- pyc2ray/domain/__init__.py
+++ pyc2ray/domain/__init__.py
@@ -1,2 +1,8 @@
 # FIXME: don't use import * to avoid namespace pollution
-from .domain_decomposition_utils import *  # noqa: F403
+from .grid import *  # noqa: F403
+from .morton_grouping import *  # noqa: F403
+from .regular_grid import *  # noqa: F403
+from .source_grouping import *  # noqa: F403
+from .sources import *  # noqa: F403
+from .subdomain import *  # noqa: F403
+from .utils import *  # noqa: F403
