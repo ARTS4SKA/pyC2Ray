c717b82a5a5839152422ea4cdac8f49fe7ad5ae3
ChristoperBignamini <christopher.bignamini@gmail.com>
2026-05-19 17:00:21 +0200

    Cost model usage added

--- pyc2ray/domain/source_grouping.py
+++ pyc2ray/domain/source_grouping.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 # import logging
 from typing import List
 
+from pyc2ray.domain.cost_model import CostModel
 from pyc2ray.domain.sources import SourceGroup
 
 # ==============================================================================
@@ -16,13 +17,12 @@ from pyc2ray.domain.sources import SourceGroup
 class GroupingParams:
     """Common parameters for all source grouping algorithms.
     """
-    def __init__(self, max_num_sources_per_group: int = 10, max_cost_per_group: float = 1.0) -> None:
+    def __init__(self, max_num_sources_per_group: int = 10) -> None:
          self.max_num_sources_per_group: int = max_num_sources_per_group
-         self.max_cost_per_group: float = max_cost_per_group
 
 class SourceGrouping:
     """Class providing a common interface for all grouping algorithms.
     """
     @abstractmethod
-    def build_groups(self, sources, grid, grouping_params: GroupingParams) -> List[SourceGroup]:
+    def build_groups(self, sources, grid, grouping_params: GroupingParams, cost_model: CostModel) -> List[SourceGroup]:
         raise NotImplementedError("Call to build_groups abstract method.")
