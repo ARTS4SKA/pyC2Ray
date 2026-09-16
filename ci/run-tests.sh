17c0f6f8cd4037574db0615f023c8a65abeaf340
Tommaso Boschi <tboschi@phys.ethz.ch>
2026-09-16 10:16:24 +0200

    fix: adding cuda 13.0 support

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -4,7 +4,10 @@ set -e
 python -m venv env
 source env/bin/activate
 
+# Install mpi4py from source to ensure it is built against the correct MPI library
+MPICC=mpicc python -m pip install --no-binary=mpi4py --no-cache-dir mpi4py
 python -m pip install .[test]
+
 pytest
 
 deactivate
